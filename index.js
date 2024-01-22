061590ce1dcb44b71d9fd203f3a725a3b810a261
NivedhaS21 <92453881+NivedhaS21@users.noreply.github.com>
2024-01-22 17:42:45 +0530

    Update index.js
    const port = process.env.PORT || 3000;
    replaced by
    const port = process.env.PORT || 4200;

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4200;
 const config = require('config')
 
 app.get('/', (req, res)=>{
